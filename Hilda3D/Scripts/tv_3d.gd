3f04e508ff94a06480edbd89e66ec5bc892b8f6f
wAIfu-DEV <w.ai.fu.dev.team@gmail.com>
2024-05-01 21:33:12 +0200

    fix duplicates

--- Hilda3D/Scripts/tv_3d.gd
+++
@@ -1,3 +0,0 @@
-extends Node3D
-
-@onready var ref_videostreamplayer: VideoStreamPlayer = $"TVVideoSubviewport/SubViewport/VideoStreamPlayer"
--- Hilda3D/StatusBar.gd
+++
@@ -1,6 +0,0 @@
-class_name StatusBar extends PanelContainer
-
-@onready var ref_label = $"Label"
-
-func displayStatus(text: String)-> void:
-    ref_label.text = text
